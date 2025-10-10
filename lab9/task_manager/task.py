d2261e2cfa47fdec58d661f64a0453a7d00478ed
Murilo Darce <104478664+mdarce765@users.noreply.github.com>
2025-10-10 16:49:18 -0300

    Update task.py

--- lab9/task_manager/task.py
+++ lab9/task_manager/task.py
@@ -6,7 +6,6 @@ Funcionalidades obrigatórias:
 4 - Atualizar status da arefa;
 5 - Deletar tarefas.
 '''
-
 from datetime import datetime
 from enum import IntEnum, Enum
 
@@ -30,3 +29,36 @@ class Atributes():
     self._prioridade = Priority
     self._prazo = datetime.now()
     self._status = Status
+
+
+def criar_tarefa():
+  return 
+def listar_tarefas():
+  return
+def buscar_tarefas():
+  return
+def atualizar_tarefas():
+  return
+def deletar_tarefas():
+  return
+def sair():
+  return
+
+def menu():
+  print("1 - Criar tarefa\n2 - Listar tarefas\n3 - Buscar tarefas\n4 - Atualizar tarefas\n5 - Deletar tarefas\n\n0 - Sair\n")
+  esc = int(input("Insira o número referente a sua escolha: "))
+  
+  if esc == 1:
+    criar_tarefa()
+  elif esc == 2:
+    listar_tarefas()
+  elif esc == 3:
+    buscar_tarefas()
+  elif esc == 4:
+    atualizar_tarefas()
+  elif esc == 5:
+    deletar_tarefas()
+  elif esc == 0:
+    sair()
+  else:
+    menu()
